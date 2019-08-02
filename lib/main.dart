a822d7ed303d9ebc777e7bc9f94b45160196c96a
Mubanga <mubangeriseme@yahoo.com>
2019-08-02 02:36:15 +0200

    Initial Commit: Write Your First Flutter App, part 1

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/** The " => " can be used if the function contains only 1 line
+ *  this IS NOT A LAMBDA!!!!!!!!!
+ */
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
